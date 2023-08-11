23875ecd2df5178548534bcd58aeea52ab3144d0
Ayoub <MobileDev@2k22>
2023-08-11 14:12:12 +0200

    setting the RecyclerView items based on the timestamp from newest to oldest.

--- app/src/main/java/com/example/firebaserecyclerview/MainActivity.java
+++ app/src/main/java/com/example/firebaserecyclerview/MainActivity.java
@@ -14,8 +14,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -51,33 +53,79 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpRecyclerView(){
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+       // Query query = databaseReference.orderByChild("timestamp"); // Assuming "timestamp" is the field name
+        Query query = databaseReference.child("users").orderByChild("timestamp").limitToLast(100); // Limiting to the last 100 records
 
-        databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 List<User> users = new ArrayList<>();
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    // ... (your existing data retrieval code)
 
+                    for (DataSnapshot keySnapshot : userSnapshot.getChildren()) {
+                        DataSnapshot firstNameSnapshot = keySnapshot.child("firstname");
+                        DataSnapshot LastNameSnapshot = keySnapshot.child("lastname");
+                        DataSnapshot agetSnapshot = keySnapshot.child("age");
+                        DataSnapshot timetampSnapshot=keySnapshot.child("timestamp");
+                        String firstname = firstNameSnapshot.getValue(String.class);
+                        String lastname = LastNameSnapshot.getValue(String.class);
+                        String age = agetSnapshot.getValue(String.class);
+                        Long timestampLong = timetampSnapshot.getValue(Long.class);
+                        long currentTimeMillis=System.currentTimeMillis();
+                        long timeElapsedMillis = currentTimeMillis - timestampLong;
+                        String dateTime=TimeElapsedCalculator.formatTimeElapsed(timeElapsedMillis,timestampLong);
+                        User user = new User(firstname, lastname, age,timestampLong,dateTime);
+                        users.add(user);
+                    }
+
+
+                }
+
+                Collections.reverse(users); // Reverse the list to display the newest first
+                if (adapter == null){
+                    adapter=new UserAdapter(users);
+                    recyclerView.setAdapter(adapter);
+                }else {
+                    adapter.updateData(users);
+                    adapter.notifyDataSetChanged();
+                }
+                Log.d("firebase", "Data : " + users);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                adapter = new UserAdapter(users);
+                recyclerView.setAdapter(adapter);
+                // ... (your existing adapter and RecyclerView setup code)
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("firebase", "Error getting data", error.toException());
+
+            }
+
+            // ... (your existing onCancelled method)
+        });
+
+    /*    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<User> users = new ArrayList<>();
                 for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                     for (DataSnapshot keySnapshot : userSnapshot.getChildren()) {
                         DataSnapshot firstNameSnapshot = keySnapshot.child("firstname");
                         DataSnapshot LastNameSnapshot = keySnapshot.child("lastname");
                         DataSnapshot agetSnapshot = keySnapshot.child("age");
                         DataSnapshot timetampSnapshot=keySnapshot.child("timestamp");
-
                         String firstname = firstNameSnapshot.getValue(String.class);
                         String lastname = LastNameSnapshot.getValue(String.class);
                         String age = agetSnapshot.getValue(String.class);
                         Long timestampLong = timetampSnapshot.getValue(Long.class);
-
                         long currentTimeMillis=System.currentTimeMillis();
                         long timeElapsedMillis = currentTimeMillis - timestampLong;
                         String dateTime=TimeElapsedCalculator.formatTimeElapsed(timeElapsedMillis,timestampLong);
-
                         User user = new User(firstname, lastname, age,timestampLong,dateTime);
                         users.add(user);
-
                     }
-
                 }
                 if (adapter == null){
                     adapter=new UserAdapter(users);
@@ -87,20 +135,15 @@ public class MainActivity extends AppCompatActivity {
                     adapter.notifyDataSetChanged();
                 }
                 Log.d("firebase", "Data : " + users);
-
                 recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                 adapter = new UserAdapter(users);
                 recyclerView.setAdapter(adapter);
-
-
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.e("firebase", "Error getting data", databaseError.toException());
             }
-        });
-
+        });*/
     }
 
     @Override
